03f3449ff70f5b023c5507cc188a66d25aa83a49
Jiazhenyu <493960474@qq.com>
2024-08-08 23:46:58 +0800

    更改了宏定义 UART 和 USB 为 _UART 和 _USB, 防止宏的重定义

--- BspLib/src/bsp_stdio_redirect.c
+++ BspLib/src/bsp_stdio_redirect.c
@@ -8,30 +8,30 @@
 #include <stdio.h>
 #include "stm32f1xx_hal.h"
 
-#define UART 0
-#define USB 1
-#define GCC 0
-#define MDK 1
+#define _UART 0
+#define _USB 1
+#define _GCC 0
+#define _MDK 1
 
 // 以下两个宏按照实际情况选择
-#define STDIO_REDIRECT_PERIPHERALS USB // UART or USB
-#define STDIO_REDIRECT_COMPILER GCC     // GCC or MDK
+#define STDIO_REDIRECT_PERIPHERALS _USB // _UART or _USB
+#define STDIO_REDIRECT_COMPILER _GCC    // _GCC or _MDK
 
 // 根据选择的外设和编译器选择对应的头文件
-#if (STDIO_REDIRECT_PERIPHERALS == UART)
+#if (STDIO_REDIRECT_PERIPHERALS == _UART)
 #include "usart.h"
 #define STDIO_REDIRECT_UART &huart1
-#elif (STDIO_REDIRECT_PERIPHERALS == USB)
+#elif (STDIO_REDIRECT_PERIPHERALS == _USB)
 #include "usbd_cdc_if.h"
 #else
 #error "Please select the peripherals for stdio redirect."
 #endif /* STDIO_REDIRECT_PERIPHERALS */
 
-#if (STDIO_REDIRECT_COMPILER == MDK)
+#if (STDIO_REDIRECT_COMPILER == _MDK)
 #include "RTE_Components.h"
 #endif /* STDIO_REDIRECT_COMPILER */
 
-#if (STDIO_REDIRECT_PERIPHERALS == USB)
+#if (STDIO_REDIRECT_PERIPHERALS == _USB)
 /**
  * @brief  通过 USB 发送数据
  * @param  pData: 要发送的数据指针
@@ -66,11 +66,11 @@ HAL_StatusTypeDef USB_Transmit(const uint8_t *pData, uint16_t Size, uint32_t Tim
 #endif /* STDIO_REDIRECT_PERIPHERALS */
 
 // 根据编译器进行不同的处理
-#if (STDIO_REDIRECT_COMPILER == MDK)
+#if (STDIO_REDIRECT_COMPILER == _MDK)
 #define PUTCHAR_PROTOTYPE int stdout_putchar(int ch)
 #define GETCHAR_PROTOTYPE int stdin_getchar(void)
 #define ERRCHAR_PROTOTYPE int stderr_putchar(int ch)
-#elif (STDIO_REDIRECT_COMPILER == GCC)
+#elif (STDIO_REDIRECT_COMPILER == _GCC)
 #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 #define GETCHAR_PROTOTYPE int __io_getchar(void)
 #define ERRCHAR_PROTOTYPE int __io_errchar(int ch)
@@ -79,13 +79,13 @@ HAL_StatusTypeDef USB_Transmit(const uint8_t *pData, uint16_t Size, uint32_t Tim
 // 标准输出
 PUTCHAR_PROTOTYPE
 {
-#if (STDIO_REDIRECT_PERIPHERALS == UART)
+#if (STDIO_REDIRECT_PERIPHERALS == _UART)
     if (HAL_UART_Transmit(&STDIO_REDIRECT_UART, (uint8_t *)&ch, 1, HAL_MAX_DELAY))
     {
         Error_Handler();
     }
     return (int)ch;
-#elif (STDIO_REDIRECT_PERIPHERALS == USB)
+#elif (STDIO_REDIRECT_PERIPHERALS == _USB)
     if (USB_Transmit((uint8_t *)&ch, 1, HAL_MAX_DELAY) != HAL_OK)
     {
         Error_Handler();
@@ -100,7 +100,7 @@ PUTCHAR_PROTOTYPE
 GETCHAR_PROTOTYPE
 {
     uint8_t ch;
-#if (STDIO_REDIRECT_PERIPHERALS == UART)
+#if (STDIO_REDIRECT_PERIPHERALS == _UART)
     if (HAL_OK == HAL_UART_Receive(&STDIO_REDIRECT_UART, &ch, 1, HAL_MAX_DELAY))
     {
         HAL_UART_Transmit(&STDIO_REDIRECT_UART, &ch, 1, HAL_MAX_DELAY); // 回显
@@ -110,7 +110,7 @@ GETCHAR_PROTOTYPE
     {
         return EOF; // 表示没有接收到字符
     }
-#elif (STDIO_REDIRECT_PERIPHERALS == USB)
+#elif (STDIO_REDIRECT_PERIPHERALS == _USB)
     // USB 输入处理代码可以在这里添加
     return (int)ch;
 #else
@@ -121,13 +121,13 @@ GETCHAR_PROTOTYPE
 // 标准错误输出
 ERRCHAR_PROTOTYPE
 {
-#if (STDIO_REDIRECT_PERIPHERALS == UART)
+#if (STDIO_REDIRECT_PERIPHERALS == _UART)
     if (HAL_UART_Transmit(&STDIO_REDIRECT_UART, (uint8_t *)&ch, 1, HAL_MAX_DELAY))
     {
         Error_Handler();
     }
     return (int)ch;
-#elif (STDIO_REDIRECT_PERIPHERALS == USB)
+#elif (STDIO_REDIRECT_PERIPHERALS == _USB)
     if (USB_Transmit((uint8_t *)&ch, 1, HAL_MAX_DELAY) != HAL_OK)
     {
         Error_Handler();
